9d5d5e83fbf2144414fdf5feea071d43b35d6f49
Aaron Krohn <akrohn@totallymoney.com>
2024-09-23 17:11:16 +0100

    use vercel cron job

--- api/postTip.js
+++ api/postTip.js
@@ -38,6 +38,10 @@ async function postTweet(content) {
 }
 
 module.exports = async function handler(req, res) {
+    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+        return res.status(401).end('Unauthorized');
+    }
+
     if (req.method === 'POST') {
         console.log('Generating and posting accessibility tip...');
         const tip = await generateAccessibilityTip();

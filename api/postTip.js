3227deece6416ee580a07b75784953a5624d20e9
Aaron Krohn <akrohn@totallymoney.com>
2024-09-23 16:45:28 +0100

    add space

--- api/postTip.js
+++ api/postTip.js
@@ -65,6 +65,7 @@ export default async function handler(req, res) {
         //     console.log('No tip generated; skipping tweet.');
         //     return res.status(200).json({ message: 'No tip generated' });
         // }
+        
     } else {
         return res.status(405).json({ message: 'Method not allowed' });
     }
